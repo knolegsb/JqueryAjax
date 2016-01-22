65c478adbdede1fc91504159f2d4992a4628f9c1
knolegsb <seon2you@hotmail.com>
2016-01-21 19:24:39 -0800

    getscript, get xml

---
+++ JqueryAjax/devlec/3_jQuerygetScript.js
@@ -0,0 +1,18 @@
+﻿var tableData = [
+    { "Num": "1", "Name": "Kani", "Title": "Hello" },
+    { "Num": "2", "Name": "Mani", "Title": "World" },
+    { "Num": "3", "Name": "Moni", "Title": "Bye" }
+];
+
+var html = "";
+html += "<table border='1'>";
+html += "<tr><th>Number</th><th>Name</th><th>Titel</th></tr>";
+$.each(tableData, function () {
+    html += "<tr>";
+    html += "<td>" + this['Num'] + "</td>";
+    html += "<td>" + this['Name'] + "</td>";
+    html += "<td>" + this['Title'] + "</td>";
+    html += "</tr>";
+});
+html += "</table>";
+$('#ctlGrid').html(html);
---
+++ JqueryAjax/devlec/FormResponseData.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JqueryAjax.devlec
+{
+    public partial class FormResponseData : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.ContentType = "text/html";
+            string html = "<div>Hello<div style=''>" + Request["Msg"] + /*"," + */ DateTime.Now.ToLongTimeString() + "</div></div>";
+            Response.Clear();
+            Response.Write(html);
+            Response.End();
+        }
+    }
+}
